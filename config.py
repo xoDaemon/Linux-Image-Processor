e86563b6dc81625d8a22912cc4a02280d4582b58
Ali <162356940+xoDaemon@users.noreply.github.com>
2024-07-11 00:46:52 +0200

    Added config file

---
+++ config.py
@@ -0,0 +1,21 @@
+import configparser
+
+class ConfigMeta(type):
+    _instances = {}
+    
+    def __call__(cls, *args, **kwds):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwds)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+    
+class Config(metaclass = ConfigMeta):
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('./config.ini')
+        
+        self.VT_API_KEY = config['VT']['VT_API_KEY']
+        self.VT_API_URL = config['VT']['VT_API_URL']
+        self.image_path = config['PATHS']['image_path']
+        self.disk_path = config['PATHS']['disk_path']
+        self.mount_path = config['PATHS']['mount_path']
--- vtscan.py
+++ vtscan.py
@@ -6,28 +6,29 @@ import fs
 init(autoreset = True)
 
 class VTscan:
-    mark_list = ["malicious", "suspicious", "harmless", "undetected", "type-unsopported"]
-    
-    def __init__(self, file : fs.Image.File):
+    def __init__(self, file : fs.Image.File, VT_API_KEY, VT_API_URL):
+        self.VT_API_KEY = VT_API_KEY
+        self.VT_API_URL = VT_API_URL
+        
         self.headers = {
-            "x-apikey" : VT_API_KEY,
+            "x-apikey" : self.VT_API_KEY,
             "User-Agent" : "python-vtscan",
             "Accept-Encoding" : "gzip, deflate",
         }
         
         self.file = file
         self.marked_as = {
-            "malicious" : True,
-            "suspicious" : True,
-            "harmless" : True,
-            "undetected" : True,
-            "type-unsopported" : True
+            "malicious" : False,
+            "suspicious" : False,
+            "harmless" : False,
+            "undetected" : False,
+            "type-unsupported" :False,
         }          
     
     def check_hash(self):
         print(f"Checking hash for {Fore.MAGENTA}{self.file.path}{Fore.RESET}...")
                 
-        upload_url = os.path.join(VT_API_URL, "files", self.file.md5_hash)
+        upload_url = os.path.join(self.VT_API_URL, "files", self.file.md5_hash)
         
         req = requests.get(url = upload_url, headers = self.headers)
         if req.status_code == 200:
@@ -39,7 +40,7 @@ class VTscan:
             av_verdicts = result.get("data").get("attributes").get("last_analysis_results")
             stats = result.get("data").get("attributes").get("last_analysis_stats")
             
-            for mark in VTscan.mark_list:
+            for mark in self.marked_as.keys():
                 if stats.get(mark) > 0:
                     self.marked_as[mark] = True
             
