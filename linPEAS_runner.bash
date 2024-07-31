fb2f67f0f33929d04777491fcb8ca77d0900cb7c
Ali <162356940+xoDaemon@users.noreply.github.com>
2024-08-01 00:07:29 +0200

    Added persistence detector

--- linPEAS_runner.bash
+++ linPEAS_runner.bash
@@ -1,29 +1,42 @@
 #!bin/bash
 
-filesystem_path=""
+filesystem_path=$1
 linpeas_log="linpeas_log.txt"
+delimiter="-------------------------------------------------------------------------"
 
 set -e
 
+echo $delimiter
+echo "Binding /mnt directory to filesystem:"
 mount --bind /mnt "$filesystem_path/mnt"
+echo "[+] Binding successful"
 
+echo $delimiter
+echo "Starting proxy server:"
 python3 ./proxy.py &
 PROXY_PID=$!
 echo $PROXY_PID
+echo "[+] Proxy server started."
 sleep 2
 
-chroot "$filesystem_path" "/bin/bash" << "EOF"
+echo $delimiter
+echo "Running linPEAS on filesystem:"
+echo $delimiter
+sleep 2
+chroot "$filesystem_path" "/bin/bash" << "EOT"
 
 cd /mnt
-curl -L localhost:8080
 curl -L "http://localhost:8080/proxy/https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh" | sh | tee "linpeas_log.txt"
 exit
 
-EOF
+EOT
 
 kill $PROXY_PID
 
 mkdir -p "./linpeas_logs"
 mv "/mnt/$linpeas_log" "./linpeas_logs/$linpeas_log"
+echo $delimiter
+echo "[+] Successfully created linPEAS log."
 
 umount "$filesystem_path/mnt"
+echo $delimiter
---
+++ persistence_detector.py
@@ -0,0 +1,31 @@
+import subprocess
+
+def persistence_detector(filesystem_path, log_name):
+    subproc = subprocess.run(["bash", "linPEAS_runner.bash", filesystem_path])
+    if subproc.returncode != 0:
+        raise subprocess.CalledProcessError(subproc.returncode, "linPEAS_runner.bash", \
+                                            "Error while executing linPEAS runner: {subproc.returncode}") from None
+    
+    pe_indicator = "[1;31;103"
+    skip_sequences = {
+        "  [1;31;103mRED/YELLOW[0m: 95% a PE vector\n",
+        "  [1;31;103mYOU ARE ALREADY ROOT!!![0m (it could take longer to complete execution)\n",
+    }
+    pe_vectors = []
+    
+    with open(f"./linpeas_logs/{log_name}", "r") as log_obj:
+        log_file = log_obj.readlines()
+        
+        for line in log_file:
+            if line in skip_sequences:
+                continue
+            if pe_indicator in line:
+                pe_vectors.append(line)
+                
+    print("LinPEAS found the following PE vectors:")
+    print("(It is recommended to review them manually)")
+    print("----------------------------------")
+    for pe_vector in pe_vectors:
+        print(pe_vector)
+    
+    print("Check out https://github.com/peass-ng/PEASS-ng/tree/master/linPEAS for more info!")
